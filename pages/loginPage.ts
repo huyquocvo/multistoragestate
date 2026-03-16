14cf853261b0e0d8015f1eafd071f072a6caf4f7
huyquocvo <quochuy29t6@gmail.com>
2026-03-16 09:43:00 +0700

    first commint

---
+++ pages/loginPage.ts
@@ -0,0 +1,34 @@
+// lib/pages/loginPage.ts
+
+import { Locator, Page } from "@playwright/test";
+
+export class LoginPage {
+  readonly username:Locator;
+  readonly password:Locator;
+  readonly submit:Locator;
+  readonly navUserMenu:Locator;
+  readonly navAdminMenu:Locator;
+  readonly navSignOut:Locator;
+  readonly navSignIn:Locator;
+constructor(private readonly page: Page) 
+    {
+    this.username = this.page.getByTestId("email");
+    this.password = this.page.getByTestId("password");
+    this.submit = this.page.getByTestId("login-submit");
+    this.navUserMenu = this.page.getByTestId("nav-menu");
+    this.navAdminMenu = this.page.getByTestId("nav-menu");
+    this.navSignOut = this.page.getByTestId("nav-sign-out");
+    this.navSignIn = this.page.getByTestId("nav-sign-in");
+    }
+  async goto() {
+    await this.page.goto("/auth/login");
+  }
+
+  async login(email: string, password: string) {
+    await this.goto();
+    await this.username.fill(email);
+    await this.password.fill(password);
+    await this.submit.click();
+  }
+
+}
---
+++ playwright.config.ts
@@ -0,0 +1,40 @@
+// playwright.config.ts
+
+import { defineConfig,devices } from "@playwright/test";
+// import type { APIRequestOptions } from "./lib/fixtures/apiRequest";
+// import { TestOptions } from "./lib/pages";
+
+require("dotenv").config();
+
+export default defineConfig({
+
+  projects: [
+    { 
+      name: "setup", testMatch: /.*\.setup\.ts/, 
+      fullyParallel: true 
+
+    },
+    {
+      name: "ui-tests",
+      use: {
+        ...devices['Desktop Chrome'],
+        //storageState: '.auth/admin.json', // Path from setup
+      },
+      dependencies: ['setup'],
+    },
+  ],
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [["html"], ["list"]],
+  use: {
+    testIdAttribute: "data-test",
+    baseURL: process.env.UI_URL,
+    //apiURL: process.env.API_URL,
+    //apiBaseURL: process.env.API_URL,
+    trace: "on",
+    headless: false,
+  },
+});
