484f64212406c1c13ebdc9813536186344563ecc
huyquocvo <quochuy29t6@gmail.com>
2026-03-17 08:53:02 +0700

    updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig,devices } from "@playwright/test";
 // import type { APIRequestOptions } from "./lib/fixtures/apiRequest";
 // import { TestOptions } from "./lib/pages";
 
-//require("dotenv").config();
+require("dotenv").config();
 
 export default defineConfig({
 
@@ -26,8 +26,8 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
   reporter: [["html"], ["list"]],
   use: {
     testIdAttribute: "data-test",
